2873205219c323530ea1eab1d9886003abda2681
Jack <jhkent88@gmail.com>
2022-04-28 14:12:37 -0700

    Add download link and spongebob pic

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          click to download item:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="./download.png"
+          download="download.png"
         >
-          Learn React
+          download
         </a>
       </header>
     </div>
