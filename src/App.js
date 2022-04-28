22a6cdaadd8e02c4ba1c8f86acfd9e643d4c526b
Jack <jhkent88@gmail.com>
2022-04-28 14:21:58 -0700

    Add download button w onClick event function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          click to download item:
-        </p>
-        <a
-          className="App-link"
-          href="./download.png"
-          download="download.png"
-        >
-          download
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    url: null
+  }
+
+  onClick = event => {
+    console.log('hello!')
+    // try code to download from S3 here
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            click to download item:
+          </p>
+          {/* <a
+            className="App-link"
+            href="./download.png"
+            download="download.png"
+          >
+            download
+          </a> */}
+          <button
+            onClick={this.onClick}
+          >
+            download
+          </button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
